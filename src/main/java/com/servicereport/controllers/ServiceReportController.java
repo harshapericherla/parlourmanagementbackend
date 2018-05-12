79726515b259e3bec8c9446b0c8a19cffa5fc5bd
harshapericherla <harsha.pericherla@gmail.com>
2018-05-12 08:58:37 +0530

    add comments for the code

--- src/main/java/com/servicereport/controllers/ServiceReportController.java
+++ src/main/java/com/servicereport/controllers/ServiceReportController.java
@@ -25,7 +25,7 @@ public class ServiceReportController {
 	@Autowired
 	private ServiceReportService serviceReportService;
 	
-	/* This method returns the list of service reports
+	/** This method returns the list of service reports
 	 * @param requestArg
 	 * @return
 	 * @throws Exception
@@ -38,7 +38,7 @@ public class ServiceReportController {
 	    return new ResponseEntity<List<ServiceReportBean>>(localServiceReportBeans,HttpStatus.OK);
 	}
 	
-	/* This method adds the serviceReport to the database 
+	/** This method adds the serviceReport to the database 
 	 * @param serviceReportBeanArg
 	 * @return
 	 */
@@ -49,6 +49,10 @@ public class ServiceReportController {
 		return new ResponseEntity<ServiceReportBean>(localServiceReportBean,HttpStatus.OK);
 	}
     
+	/** This method is used to edit the serviceReport
+	 * @param serviceReportBeanArg
+	 * @return
+	 */
 	@RequestMapping(value="/editServiceReport",method = RequestMethod.POST)
 	public ResponseEntity<?> editServiceReport(@RequestBody ServiceReportBean serviceReportBeanArg){
 		
@@ -56,6 +60,10 @@ public class ServiceReportController {
 		return new ResponseEntity<ServiceReportBean>(localServiceReportBean,HttpStatus.OK);
 	}
 	
+	/** This method is used to delete the serviceReport
+	 * @param serviceReportBeanArg
+	 * @return
+	 */
 	@RequestMapping(value="/deleteServiceReport",method = RequestMethod.POST)
 	public ResponseEntity<?> deleteServiceReport(@RequestBody ServiceReportBean serviceReportBeanArg){
 		
